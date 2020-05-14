3e2bcf9ef6ec4c88ff50941bfecab9a3fcc8b223
Joalbert <joalbertgonzalez@gmail.com>
2020-05-14 00:13:27 -0500

    [ADD] events added

--- fractal.py
+++ fractal.py
@@ -22,14 +22,20 @@ bg = 25, 25, 25
 # Pintamos el fondo con el color elegido.
 screen.fill(bg)
 
-common = 450
-nxC, nyC = common, common
+nxC, nyC = 500, 500
 
 dimCW = width / nxC
 dimCH = height / nyC
 
 # Estado de las celdas. Vivas = 1, Muertas = 0.
 gameState = np.zeros((nxC, nyC))
+
+def __incNyC__(gameState):
+    gameState = np.zeros((nxC, nyC))
+    gameState[int(nxC / 2), 0] = 1
+    screen.fill(bg)
+
+    return gameState
 
 gameState[int(nxC / 2), 0] = 1
 # Para regla 110
@@ -41,14 +47,20 @@ gameState[int(nxC / 2), 0] = 1
 running = True
 pauseExect = False
 sleep = 0.01
-# rule = 30
+rule = 30
 # rule = 110
 # rule = 124
 # rule = 150
-rule = 90
-strRule = np.binary_repr(rule, 8)
-vectorRule = np.flip(list(strRule))
+# rule = 90
+def __log__(msg):
+    print('Log:', msg)
 
+def __binary_rule__(rule):
+    strRule = np.binary_repr(rule, 8)
+    __log__(rule)
+    return np.flip(list(strRule))
+
+vectorRule = __binary_rule__(rule)
 def exit_event(ev):
     for event in ev:
         # Detectamos si se presiona esc o hacen click sobre close.
@@ -57,14 +69,14 @@ def exit_event(ev):
             pygame.quit()
             exit()
 
-y = 0
+x, y = 0, 0
 # Para seguir calculando y pintando al reanudar.
 auxY, auxX = 0, 0
 painted = False
 from sys import exit
 # Bucle de ejecición.
 # while True:
-while y < nxC or running:
+while y < nyC or running:
     # Detenemos la ejecución si ya se terminó de pintar.
     if y == nyC:
         painted = True
@@ -73,7 +85,49 @@ while y < nxC or running:
 
     exit_event(ev)
 
-    if not painted:
+    restart = False
+    for event in ev:
+        if event.type == pygame.KEYDOWN:
+            key = event.key
+
+            if key == pygame.K_UP or key == pygame.K_DOWN or key == pygame.K_LEFT or key == pygame.K_RIGHT \
+                    or key == pygame.K_n or key == pygame.K_m:
+                restart = True
+                mods = pygame.key.get_mods()
+
+                if key == pygame.K_UP:
+                    nyC = nyC + 10
+                    dimCH = height / nyC
+                elif key == pygame.K_DOWN:
+                    nyC = nyC - 10
+                    dimCH = height / nyC
+                elif key == pygame.K_LEFT:
+                    nxC = nxC - 10
+                    dimCW = width / nxC
+                elif key == pygame.K_RIGHT:
+                    nxC = nxC + 10
+                    dimCW = width / nxC
+                elif key == pygame.K_n:
+                    if mods & pygame.KMOD_LSHIFT or mods & pygame.KMOD_CAPS:
+                        rule += 1
+                    else:
+                        rule -= 1
+
+                    vectorRule = __binary_rule__(rule)
+                elif key == pygame.K_m:
+                    if mods & pygame.KMOD_LSHIFT or mods & pygame.KMOD_CAPS:
+                        rule += 10
+                    else:
+                        rule -= 10
+
+                    vectorRule = __binary_rule__(rule)
+
+                painted = False
+                y = 0
+                auxY = 0
+                gameState = __incNyC__(gameState)
+
+    if not painted or not restart:
         for event in ev:
             # Detectamos si se presiona una tecla.
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
@@ -88,17 +142,17 @@ while y < nxC or running:
         if not pauseExect:
             time.sleep(sleep)
 
-            for x in range(0, nyC):
+            for x in range(0, nxC):
                 # Encontramos los vecinos cercanos, (x-1, y)/(x+1, y).
-                xLeft =  int(gameState[(x - 1) % nxC, (y) % nyC])
-                xOrigin = int(gameState[x % nxC, y % nyC])
-                xRight = int(gameState[(x + 1) % nxC, (y) % nyC])
+                xLeft   = int(gameState[(x - 1) % nxC, (y) % nyC])
+                xOrigin = int(gameState[(x)     % nxC, (y) % nyC])
+                xRight  = int(gameState[(x + 1) % nxC, (y) % nyC])
 
                 vicinity = "%s%s%s" % (xLeft, xOrigin, xRight)
                 cell = int(vectorRule[int(vicinity, 2)])
 
                 if cell:
-                    gameState[x, (y + 1) % nyC] = 1
+                    gameState[(x) % nxC, (y + 1) % nyC] = 1
 
                     # Creamos el polígono de cada celda a dibujar.
                     poly = [((x)     * dimCW, y       * dimCH),
